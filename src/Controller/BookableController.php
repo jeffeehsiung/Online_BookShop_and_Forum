195065844e5c1a462b9c8e5584ebad4c6310b2c9
kamiel.sabo <kamiel.sabo@student.kuleuven.be>
2023-04-23 10:38:58 +0200

    Basic clean up base.html.twig, more rerfactoring needed for integrating javascript files with encore

--- src/Controller/BookableController.php
+++ src/Controller/BookableController.php
@@ -18,7 +18,7 @@ class BookableController extends AbstractController
     {
         // TODO: split twig templates into file format 'controllername/methodname.html.twig' -> example: 'bookable/settings.html.twig'
         return $this->render('setting.html.twig',[
-            'username' => 'test_user', // TODO: edit once user login has been implemented
+            'username' => 'test_user',
             'stylesheets' => 'settings.css'
         ]);
     }
