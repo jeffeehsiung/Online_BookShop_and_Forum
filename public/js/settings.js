df50f9f3a21c1973613f6132a2ddb6260fc5eb5b
kamiel.sabo <kamiel.sabo@student.kuleuven.be>
2023-04-23 18:05:07 +0200

    Basic clean up base.html.twig, more rerfactoring needed for integrating javascript files with encore

---
+++ public/js/settings.js
@@ -0,0 +1,16 @@
+let coll = document.getElementsByClassName("settings-submenu");
+let settings = document.getElementById("settings");
+
+settings.style.width = document.getElementById("navbar").offsetWidth.toString() + "px";
+
+for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        let content = this.nextElementSibling;
+        if (content.style.display === "flex") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "flex";
+        }
+    });
+} 
--- src/Controller/BookableController.php
+++ src/Controller/BookableController.php
@@ -8,17 +8,38 @@ use function Symfony\Component\String\u;
 
 class BookableController extends AbstractController
 {
-    private array $stylesheets;
-
-    public function __construct() {
-        $this->stylesheets[] = 'home.css';
-    }
     #[Route('/settings')]
     public function settings(): Response
     {
         // TODO: split twig templates into file format 'controllername/methodname.html.twig' -> example: 'bookable/settings.html.twig'
+        $stylesheets = ['settings.css'];
+        $javascripts = ['settings.js'];
+        $bookGenres = array(
+            "Fiction",
+            "Mystery",
+            "Romance",
+            "Science Fiction",
+            "Fantasy",
+            "Thriller",
+            "Biography",
+            "History",
+            "Self-help",
+            "Horror",
+            "Cooking",
+            "Travel",
+            "Art",
+            "Business",
+            "Religion",
+            "Humor",
+            "Children's",
+            "Young Adult"
+        );
+
         return $this->render('setting.html.twig',[
-            'username' => 'test_user'
+            'username' => 'test_user',
+            'stylesheets' => $stylesheets,
+            'javascripts' => $javascripts,
+            'bookgenres' => $bookGenres
         ]);
     }
 
