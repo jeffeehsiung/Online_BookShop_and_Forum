996e6f175447e9c75c766103fbbf3a16931a1c12
martijn.leemans <martijn.leemans@student.kuleuven.be>
2023-05-02 10:45:03 +0200

    further styling the buttons

--- public/js/welcome.js
+++ public/js/welcome.js
@@ -6,11 +6,13 @@ function LoginFilledIn(){
     if (document.getElementById("login_username").value!=="" && document.getElementById("login_password").value!==""){
         document.getElementById("login_submit").disabled=false;
         document.getElementById("login_submit").style.opacity="1";
+        document.getElementById("login_submit").style.cursor="pointer";
     }
     else
     {
         document.getElementById("login_submit").disabled=true;
         document.getElementById("login_submit").style.opacity="0.6";
+        document.getElementById("login_submit").style.cursor="not-allowed";
     }
 }
 
@@ -19,11 +21,13 @@ function RegisterFilledIn(){
         && document.getElementById("email").value!==""){
         document.getElementById("register_submit").disabled=false;
         document.getElementById("register_submit").style.opacity="1";
+        document.getElementById("register_submit").style.cursor="pointer";
     }
     else
     {
         document.getElementById("register_submit").disabled=true;
         document.getElementById("register_submit").style.opacity="0.6";
+        document.getElementById("register_submit").style.cursor="not-allowed";
     }
 }
 
