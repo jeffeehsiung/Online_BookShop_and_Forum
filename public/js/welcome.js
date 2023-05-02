9810423d79e31f56a0cef8bb342b782eace1b87f
martijn.leemans <martijn.leemans@student.kuleuven.be>
2023-05-02 09:45:54 +0200

    Javascript is now done through a seperate file rather than in the html. Width isn't parsed through javascript either.

---
+++ public/js/welcome.js
@@ -0,0 +1,14 @@
+function loginOn() {
+    document.getElementById("Login").style.display = "flex";
+}
+
+function loginOff() {
+    document.getElementById("Login").style.display = "none";
+}
+function registerOn() {
+    document.getElementById("Register").style.display = "flex";
+}
+
+function registerOff() {
+    document.getElementById("Register").style.display = "none";
+}
