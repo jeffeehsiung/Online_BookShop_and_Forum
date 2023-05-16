924e3777501448b04dae77302a2686b8dac64844
jens <jens.coosemans@student.kuleuven.be>
2023-05-16 04:05:14 +0200

    fix typo

--- src/Entity/LikedGenre.php
+++ src/Entity/LikedGenre.php
@@ -6,7 +6,7 @@ use App\Repository\LikedGenreRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: LikedGenreRepository::class)]
-#[ORM\Table(name: 'a22web12.liked_genres')]
+#[ORM\Table(name: 'a22web12.liked_genre')]
 class LikedGenre
 {
     #[ORM\Id]
