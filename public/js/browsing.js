491cc2b022fe0a8f1753a57fa6dc540489a7f8c4
JeffeeH <j22132235@gmail.com>
2023-05-20 17:10:24 +0200

    partially updated search btn function

--- public/js/browsing.js
+++ public/js/browsing.js
@@ -4,6 +4,8 @@ let search_btn = document.getElementById("search-btn");
 let genre_filters = document.getElementsByClassName("genre-filter");
 let details_btn = document.getElementById("details-btn");
 
+// add event listener to the search button
+search_btn.addEventListener("click", search);
 
 // declare a function to be called when the user clicks on search button
 function search() {
@@ -17,10 +19,33 @@ function search() {
         }
     }
     // get the book name value
-    let book_name_value = book_name.value;
-
-    // redirect to the browsing page with the selected genres and book name (URL)
-    window.location.href = "/browsing?genres=" + selected_genres.join(",") + "&book_name=" + book_name_value;
+    let book_name_value = book_name.value.trim();
+    // fetch
+    fetch("/browsing", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            genres: selected_genres,
+            book_name: book_name_value
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        // if there is an error, display the error message
+        if (data.error) {
+            alert(data.error);
+        }
+        // if there is no error, redirect to the browsing page with the selected genres and book name (URL)
+        else {
+            window.location.href = "/browsing?genres=" + selected_genres.join(",") + "&book_name=" + book_name_value;
+        }
+    })
+    .catch(error => {
+        console.log(error);
+    }
+    );
 }
 
 // declare a function to be called when the user clicks on a genre filter
