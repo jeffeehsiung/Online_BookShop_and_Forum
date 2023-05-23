0249c4cd5064ee105ab3a4e82840e98e7f393b1c
jens <jens.coosemans@student.kuleuven.be>
2023-05-23 09:06:44 +0200

    start working on fixtures

---
+++ src/DataFixtures/LikedBookFixtures.php
@@ -0,0 +1,16 @@
+<?php
+
+// src/DataFixtures/AppFixtures.php
+namespace App\DataFixtures;
+
+use App\Entity\Product;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+
+    }
+}
