c1c816cc8edd00fbe48d950c51d353cf305fd9f8
jens <jens.coosemans@student.kuleuven.be>
2023-05-23 18:29:48 +0200

    Add fixtures for likes dislikes and following

--- src/DataFixtures/DislikedBooksFixtures.php
+++ src/DataFixtures/DislikedBooksFixtures.php
@@ -9,15 +9,16 @@ use App\Repository\BookRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
-class LikedBooksFixtures extends Fixture
+class DislikedBooksFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        //set user with userID = 1006 == Jens
+        //set id equal to the one you actually want to use
         $user = $manager->getRepository(User::class)->findOneBy(['id' => 1006]);
         //add the first 100 books in the database to the likedbooks for a specific user
         for ($i = 0; $i < 100; $i++) {
-            $book = $manager->getRepository(Book::class)->findOneBy(['id' => $i+1]);
+            //make sure that liked and disliked don't overlap!!!!
+            $book = $manager->getRepository(Book::class)->findOneBy(['id' => $i+101]);
             $likedBook = new LikedBook();
             $likedBook->setBook($book);
             $likedBook->setUser($user);
--- src/DataFixtures/FollowedBooksFixtures.php
+++ src/DataFixtures/FollowedBooksFixtures.php
@@ -9,15 +9,15 @@ use App\Repository\BookRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
-class LikedBooksFixtures extends Fixture
+class FollowedBooksFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
         //set user with userID = 1006 == Jens
         $user = $manager->getRepository(User::class)->findOneBy(['id' => 1006]);
-        //add the first 100 books in the database to the likedbooks for a specific user
+        //add the first 100 uneven books to the database for a specific user
         for ($i = 0; $i < 100; $i++) {
-            $book = $manager->getRepository(Book::class)->findOneBy(['id' => $i+1]);
+            $book = $manager->getRepository(Book::class)->findOneBy(['id' => 2*$i+1]);
             $likedBook = new LikedBook();
             $likedBook->setBook($book);
             $likedBook->setUser($user);
