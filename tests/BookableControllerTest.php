bda9aee499b3a52f540a99931fdb548a9437fba4
JeffeeH <j22132235@gmail.com>
2023-05-27 22:18:16 +0200

    integration test template set up

--- tests/BookableControllerTest.php
+++ tests/BookableControllerTest.php
@@ -3,6 +3,11 @@
 namespace App\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use App\Repository\GenreRepository;
+use App\Repository\BookRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class BookableControllerTest extends WebTestCase
 {
@@ -87,11 +92,38 @@ class BookableControllerTest extends WebTestCase
 
     public function testBrowsing()
     {
+        // Create a new client to browse the application
         $client = static::createClient();
-        $client->request('GET', '/browsing/book-title');
-
+        $crawler = $client->request('GET', '/welcome');
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertSelectorTextContains('.title', 'Browsing');
-        // Add more assertions based on the expected behavior of the browsing route
+        // log in
+        $form = $crawler->selectButton('Submit')->form();
+        $form['_username'] = 'jeffee@gmail.com';
+        $form['_password'] = 'jeffee';
+        $client->submit($form);
+
+         print_r($client->getResponse()->getContent());
+
+//        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+//        $this->assertSelectorTextContains('.title', 'Browsing');
+//
+//        // Check if book details are displayed correctly
+//        $this->assertCount(1, $crawler->filter('.book-title'));
+//        $this->assertEquals('Book Title', $crawler->filter('.book-title')->text());
+//        $this->assertCount(1, $crawler->filter('.book-author'));
+//        $this->assertEquals('Book Author', $crawler->filter('.book-author')->text());
+//        // Add more assertions based on the expected book details
+//
+//        // Check if related books are displayed correctly
+//        $this->assertCount(3, $crawler->filter('.related-book'));
+//        // Add more assertions based on the expected related books
+//
+//        // Check if book categories are displayed correctly
+//        $this->assertCount(2, $crawler->filter('.category'));
+//        // Add more assertions based on the expected categories
+//
+//        // Check if book reviews are displayed correctly
+//        $this->assertCount(2, $crawler->filter('.book-review'));
+//        // Add more assertions based on the expected reviews
     }
 }
