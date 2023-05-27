7fb0a7848d3e18d0aa331d7c5a384a14f8745b26
martijn.leemans <martijn.leemans@student.kuleuven.be>
2023-05-27 14:24:06 +0200

    test mail working but authentication mail nto due to messenger

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -49,12 +49,12 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('bookablea22web12@gmail.com', 'bookable automated mail'))
+                    ->from(new Address('bookablea21web12@gmail.com', 'bookable automated mail'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
             );
-            // do anything else you need here, like send an email
+
 
             return $this->redirectToRoute('home');
         }
