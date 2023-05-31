eb55711fb5c9dd13815c809f73a9d8e3f19e468d
martijn.leemans <martijn.leemans@student.kuleuven.be>
2023-05-31 12:35:55 +0200

    registration is completely tested

--- tests/Integration/Controller/RegistrationControllerTest.php
+++ tests/Integration/Controller/RegistrationControllerTest.php
@@ -41,7 +41,7 @@ class RegistrationControllerTest extends WebTestCase
         $form['registration_form[plainPassword][second]'] = "password";
         $form["registration_form[agreeTerms]"] = true;
         $client->submit($form);
-        $crawler = $client->followRedirect();
+        $crawler = $client->followRedirect(); //2 redirections because it counts the "going to welcome" as a seperate redirect
         $crawler = $client->followRedirect();
         //make sure we got to the welcome page
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
@@ -52,5 +52,74 @@ class RegistrationControllerTest extends WebTestCase
         $testRegisterObject = $userManager->getRepository(User::class)->findOneBy(['email' => "testregister@test.com"]);
         $userManager->remove($testRegisterObject);
         $userManager->flush();
+
+
+        /*
+         * test registration without accepting the terms
+         */
+
+        //click the register link
+        $linkPosition = 4;
+        $link = $crawler->filter('a')->eq($linkPosition); //of all the links, register is the fourth one
+        $crawler = $client->click($link->link());
+        //make sure we are on the register page
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertSelectorTextContains('title', 'Register');
+
+        //fill in the form
+        $form = $crawler->filter('form[name="registration_form"]')->form();
+        $fields = $form->all();
+        $fieldNames = array_keys($fields);
+        $form['registration_form[first_name]'] = "testname";
+        $form['registration_form[last_name]'] = "testname";
+        $form['registration_form[username]'] = "testname";
+        $form['registration_form[email]'] = "testregister@test.com";
+        $form['registration_form[plainPassword][first]'] = "password";
+        $form['registration_form[plainPassword][second]'] = "password";
+        $client->submit($form);
+        //make sure we remained on the registration page
+        $this->assertSelectorTextContains('title', 'Register');
+
+        /*
+         * test registration with mismatching password
+         */
+
+        //fill in the form
+        $form = $crawler->filter('form[name="registration_form"]')->form();
+        $fields = $form->all();
+        $fieldNames = array_keys($fields);
+        $form['registration_form[first_name]'] = "testname";
+        $form['registration_form[last_name]'] = "testname";
+        $form['registration_form[username]'] = "testname";
+        $form['registration_form[email]'] = "testregister@test.com";
+        $form['registration_form[plainPassword][first]'] = "password";
+        $form['registration_form[plainPassword][second]'] = "wrongPassword";
+        $form["registration_form[agreeTerms]"] = true;
+        $client->submit($form);
+
+        //make sure we remained on the register page
+        $this->assertSelectorTextContains('title', 'Register');
+        //check that the user gets the text showing that their passwords don't match
+        $this->assertSelectorTextContains('#register_fields > div > ul > li', 'The password fields must match.');
+
+
+        /*
+         * test that you can't register if any of the regular fields are not filled in
+         */
+        //fill in the form
+        $form = $crawler->filter('form[name="registration_form"]')->form();
+        $fields = $form->all();
+        $fieldNames = array_keys($fields);
+        $form['registration_form[first_name]'] = "";
+        $form['registration_form[last_name]'] = "testname";
+        $form['registration_form[username]'] = "testname";
+        $form['registration_form[email]'] = "testregister@test.com";
+        $form['registration_form[plainPassword][first]'] = "password";
+        $form['registration_form[plainPassword][second]'] = "wrongPassword";
+        $form["registration_form[agreeTerms]"] = true;
+        $client->submit($form);
+
+        //make sure we remained on the register page
+        $this->assertSelectorTextContains('title', 'Register');
     }
 }
